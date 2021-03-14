d183575cab1cad332551e6702c3a9846334b7708
Marx Bitware <info@marxbitware.co.uk>
2021-03-14 20:10:39 +0700

    Moves the key from the command line to a prompt.

--- Vernam512/Program.cs
+++ Vernam512/Program.cs
@@ -15,7 +15,7 @@ namespace Vernam512
 
     class Program
     {
-        private static string version = "1.0.1";
+        private static string version = "1.0.2";
 
         static void Main(string[] args)
         {
@@ -28,6 +28,7 @@ namespace Vernam512
                 {
                     Console.WriteLine("Vernam512 Cipher " + version);
                     Console.WriteLine(Environment.NewLine);
+                    v.Key = GetKey();
                     File.WriteAllBytes(v.OutputFile, VernamHash.Encrypt(v.Key, v.InputFile, v.ByteSelection));
                     Console.WriteLine("Encrypted.");
                 }
@@ -35,6 +36,7 @@ namespace Vernam512
                 {
                     Console.WriteLine("Vernam512 Cipher " + version);
                     Console.WriteLine(Environment.NewLine);
+                    v.Key = GetKey();
                     File.WriteAllBytes(v.OutputFile, VernamHash.Decrypt(v.Key, v.InputFile, v.ByteSelection));
                     Console.WriteLine("Decrypted.");
                 }
@@ -47,14 +49,14 @@ namespace Vernam512
 
         private static Variables ParseArgs(string[] args)
         {
-            //Vernam512 -e -k abc123445 -i plaintext.txt -o ciphertext.512 -b 0
-            //Vernam512 -d -k abc123445 -i ciphertext.512 -o plaintext.txt -b 0
+            //Vernam512 -e -i plaintext.txt -o ciphertext.512 -b 0
+            //Vernam512 -d -i ciphertext.512 -o plaintext.txt -b 0
             //Vernam512 -h
 
-            if(args.Length == 9 && args[0] == "-e")
-                return new Variables() { IsEncrypt = true, InputFile = File.ReadAllBytes(args[4]), Key = Encoding.Unicode.GetBytes(args[2]), OutputFile = args[6] };
-            else if (args.Length == 9 && args[0] == "-d")
-                return new Variables() { IsEncrypt = false, InputFile = File.ReadAllBytes(args[4]), Key = Encoding.Unicode.GetBytes(args[2]), OutputFile = args[6] };
+            if(args.Length == 7 && args[0] == "-e")
+                return new Variables() { IsEncrypt = true, InputFile = File.ReadAllBytes(args[2]), OutputFile = args[4], ByteSelection = int.Parse(args[6]) };
+            else if (args.Length == 7 && args[0] == "-d")
+                return new Variables() { IsEncrypt = false, InputFile = File.ReadAllBytes(args[2]), OutputFile = args[4], ByteSelection = int.Parse(args[6]) };
             else if (args.Length == 1 && args[0] == "-h")
             {
                 Console.WriteLine("Vernam512 Cipher " + version);
@@ -69,8 +71,6 @@ namespace Vernam512
                 Console.WriteLine("-d                   - Encrypt");
                 Console.WriteLine("-e                   - Decrypt");
                 Console.WriteLine("-h                   - Display help");
-                Console.WriteLine("-i (filename)        - input file");
-                Console.WriteLine("-k (key)             - Key - Must be 64 bytes or larger");
                 Console.WriteLine("-o (filename)        - output file");
                 Environment.Exit(0);
             }
@@ -90,12 +90,6 @@ namespace Vernam512
 
             if(string.IsNullOrEmpty(v.OutputFile))
                 Terminate("Missing output file");
-
-            if (v.Key == null)
-                Terminate("No key");
-
-            if (v.Key.Length < 64)
-                Terminate("The key supplied was under 64 bytes.");
         }
 
         private static void Terminate(string message)
@@ -105,6 +99,26 @@ namespace Vernam512
             Console.WriteLine(message);
             Console.WriteLine("Incorrect usage.  Please type 'Vernam512 -h' for help.");
             Environment.Exit(0);
+        }
+
+        private static byte[] GetKey()
+        {
+            byte[] key = null;
+
+            while (key == null)
+            {
+                Console.WriteLine("Please input key and press ENTER when finished:");
+                string result = Console.ReadLine();
+
+                byte[] r = Encoding.Unicode.GetBytes(result);
+
+                if (r.Length >= 64)
+                    key = r;
+                else
+                    Console.WriteLine("Please enter a larger key (64 characters or more).");
+            }
+
+            return key;
         }
     }
 }
