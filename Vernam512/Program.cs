77d90261ebbec6b0d38a2bd796ad07a11fc35c4a
Marx Bitware <info@marxbitware.co.uk>
2021-03-15 10:11:01 +0700

    Better handling of input args, optional key in the command line

--- Vernam512/Program.cs
+++ Vernam512/Program.cs
@@ -15,7 +15,7 @@ namespace Vernam512
 
     class Program
     {
-        private static string version = "1.0.3";
+        private static string version = "1.0.4";
 
         static void Main(string[] args)
         {
@@ -28,7 +28,12 @@ namespace Vernam512
                 {
                     Console.WriteLine("Vernam512 Cipher " + version);
                     Console.WriteLine(Environment.NewLine);
-                    v.Key = GetKey();
+
+                    if (v.Key == null)
+                        v.Key = GetKey();
+                    else if (v.Key.Length < 64)
+                        Terminate("Key is too short.");
+
                     File.WriteAllBytes(v.OutputFile, VernamHash.Encrypt(v.Key, v.InputFile, v.ByteSelection));
                     Console.WriteLine("Encrypted.");
                 }
@@ -36,7 +41,12 @@ namespace Vernam512
                 {
                     Console.WriteLine("Vernam512 Cipher " + version);
                     Console.WriteLine(Environment.NewLine);
-                    v.Key = GetKey();
+
+                    if (v.Key == null)
+                        v.Key = GetKey();
+                    else if (v.Key.Length < 64)
+                        Terminate("Key is too short.");
+
                     File.WriteAllBytes(v.OutputFile, VernamHash.Decrypt(v.Key, v.InputFile, v.ByteSelection));
                     Console.WriteLine("Decrypted.");
                 }
@@ -51,33 +61,74 @@ namespace Vernam512
         {
             //Vernam512 -e -i plaintext.txt -o ciphertext.512 -b 0
             //Vernam512 -d -i ciphertext.512 -o plaintext.txt -b 0
+            //Vernam512 -e -k abc12345 -i plaintext.txt -o ciphertext.512 -b 0
+            //Vernam512 -d -k abc12345 -i ciphertext.512 -o plaintext.txt -b 0
             //Vernam512 -h
 
-            if(args.Length == 7 && args[0] == "-e")
-                return new Variables() { IsEncrypt = true, InputFile = File.ReadAllBytes(args[2]), OutputFile = args[4], ByteSelection = int.Parse(args[6]) };
-            else if (args.Length == 7 && args[0] == "-d")
-                return new Variables() { IsEncrypt = false, InputFile = File.ReadAllBytes(args[2]), OutputFile = args[4], ByteSelection = int.Parse(args[6]) };
-            else if (args.Length == 1 && args[0] == "-h")
-            {
-                Console.WriteLine("Vernam512 Cipher " + version);
-                Console.WriteLine(Environment.NewLine);
-                Console.WriteLine("To encrypt:");
-                Console.WriteLine("Vernam512 -e -i plaintext.txt -o ciphertext.512 -b 0");
-                Console.WriteLine("To decrypt:");
-                Console.WriteLine("Vernam512 -d -i ciphertext.512 -o plaintext.txt -b 0");
-                Console.WriteLine(Environment.NewLine);
-                Console.WriteLine("Parameters:");
-                Console.WriteLine("-b                   - Byte selection (0-63)");
-                Console.WriteLine("-d                   - Encrypt");
-                Console.WriteLine("-e                   - Decrypt");
-                Console.WriteLine("-h                   - Display help");
-                Console.WriteLine("-o (filename)        - output file");
-                Environment.Exit(0);
-            }
-            else
-                Terminate("Incorrect parameters are being supplied");
+            Variables v = new Variables();
 
-            return null;  //Should never get here
+            if (args.Length == 7 || args.Length == 9 || args.Length == 1)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i] == "-e")
+                        v.IsEncrypt = true;
+
+                    if (args[i] == "-d")
+                        v.IsEncrypt = false;
+
+                    if (args[i] == "-b")
+                    {
+                        v.ByteSelection = int.Parse(args[i + 1]);
+                        i++;
+                    }
+
+                    if (args[i] == "-h" || args[i] == "-help")
+                    {
+                        Console.WriteLine("Vernam512 Cipher " + version);
+                        Console.WriteLine(Environment.NewLine);
+                        Console.WriteLine("To encrypt:");
+                        Console.WriteLine("Vernam512 -e [-k abc12345] -i plaintext.txt -o ciphertext.512 -b 0");
+                        Console.WriteLine("To decrypt:");
+                        Console.WriteLine("Vernam512 -d [-k abc12345] -i ciphertext.512 -o plaintext.txt -b 0");
+                        Console.WriteLine("[...] is an optional parameter.");
+                        Console.WriteLine(Environment.NewLine);
+                        Console.WriteLine("Parameters:");
+                        Console.WriteLine("-b                   - Byte selection (0-63)");
+                        Console.WriteLine("-d                   - Encrypt");
+                        Console.WriteLine("-e                   - Decrypt");
+                        Console.WriteLine("-h                   - Display help");
+                        Console.WriteLine("-i (filename)        - Input file");
+                        Console.WriteLine("-k (Key)             - Key (optional). 64 bytes or more.");
+                        Console.WriteLine("-o (filename)        - Output file");
+                        Environment.Exit(0);
+                    }
+
+                    if (args[i] == "-k")
+                    {
+                        v.Key = Encoding.Unicode.GetBytes(args[i + 1]);
+                        i++;
+                    }
+
+                    if (args[i] == "-i")
+                    {
+                        v.InputFile = File.ReadAllBytes(args[i + 1]);
+                        i++;
+                    }
+
+                    if (args[i] == "-o")
+                    {
+                        v.OutputFile = args[i + 1];
+                        i++;
+                    }
+                }
+            } 
+            else
+            {
+                Terminate("Invalid number of parameters.");
+            }
+
+            return v;
         }
 
         private static void ValidateVariables(Variables v)
